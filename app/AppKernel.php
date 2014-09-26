83b89c8a3ae4bb5f55996837d171ee077fcfa22d
Jens <JensBreed@gmx.de>
2014-09-26 21:44:45 +0200

    add TaskBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Acme\StoreBundle\AcmeStoreBundle(),
+            new Acme\TaskBundle\AcmeTaskBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
